e3a99779c96be5f912e91441b1ef96036dc083e7
Tiago Dall'Igna <tiago@dalligna.com>
2020-09-02 21:39:36 +0200

    bugfix: calculating wrong when user didnt do a round

--- client/src/components/Scoreboard.svelte
+++ client/src/components/Scoreboard.svelte
@@ -29,8 +29,11 @@
                 <tr>
                     <td>Totals:</td>
                     {#each game.players as player}
-                        <td>{scores.reduce((r,x)=>{ 
-                            return r + x.scores.filter(x => x.player == player)[0].score
+                        <td>{scores.reduce((r,x)=>{
+                            let score = x.scores.filter(x => x.player == player)[0] 
+                                ? x.scores.filter(x => x.player == player)[0].score 
+                                : 0
+                            return r + score
                         }, 0)}</td>
                     {/each}
                 </tr>
